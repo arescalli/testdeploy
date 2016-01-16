246782d0063b99f431683bbb6154f4f9802c3bda
Alessandro Rescalli <alessandro@softrail.it>
2016-01-16 14:17:27 +0100

    pic consumer with log

--- app/consumers/pic_consumer.rb
+++ app/consumers/pic_consumer.rb
@@ -6,5 +6,6 @@ class PicConsumer
     # mark_payment_as_failed(message[:id])
     # PicImporterWorker.new.execute('test')
     puts 'Sono il pic importer!'
+    Rails.logger.info 'Sono il pic importerrrrrr'
   end
 end
