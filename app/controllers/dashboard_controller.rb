77d98ed1cd85b0f66dbd5f78c22d344887393143
Alessandro Rescalli <alessandro@softrail.it>
2016-01-16 15:14:01 +0100

    can publish messages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,19 @@
 class DashboardController < ApplicationController
   def index
-  	
+
+  end
+
+  def send_message
+  	message = {
+  		id: 1, 
+  		trains: [
+  			{number: '10101', date: Date.today},
+  			{number: '14552', date: Date.tomorrow}
+  		]
+  	}
+  	Hutch.connect
+  	Hutch.publish('logishift.pic_importer', message)
+  	Hutch.publish('gc.ps.payment.failed', message)
+  	render json: message
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
+  post 'dashboard/send_message' => 'dashboard#send_message' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
