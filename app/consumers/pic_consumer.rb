9b418371821549b37dcf3f332367d474e3f79363
Alessandro Rescalli <alessandro@softrail.it>
2016-01-17 14:31:42 +0100

    consumer with sleep

--- app/consumers/pic_consumer.rb
+++ app/consumers/pic_consumer.rb
@@ -5,9 +5,10 @@ class PicConsumer
   def process(message)
     # mark_payment_as_failed(message[:id])
     # PicImporterWorker.new.execute('test')
-    5000.times do 
+    20.times do 
     	o = OpenStruct.new(train: 1, date: Date.today)
     	Rails.logger.info 'Sono il pic importerrrrrr'
+    	sleep(10)
     end
   end
 end
