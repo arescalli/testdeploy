6cb3c37226b1d8644ef4fc347e7bb8cfd1d29230
Alessandro Rescalli <alessandro@softrail.it>
2016-01-17 18:54:28 +0100

    pic consumer write to active record

--- app/consumers/pic_consumer.rb
+++ app/consumers/pic_consumer.rb
@@ -9,6 +9,9 @@ class PicConsumer
     2000.times do 
     	o = OpenStruct.new(train: 1, date: Date.today)
     	array << o
+    	p = PicImporterLog.new
+    	p.text = Date.today.to_s
+    	p.save!
     end
     Rails.logger.info 'Sono il pic importerrrrrr'
   end
